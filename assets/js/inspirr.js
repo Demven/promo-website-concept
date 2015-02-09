29e959bd0cc5d05c0888995f9b4323ce5d0c7194
Dzmitry_Salnikau <Dzmitry_Salnikau@epam.com>
2015-02-09 13:13:04 +0300

    Modules structure and eventsService

--- assets/js/inspirr.js
+++ assets/js/inspirr.js
@@ -1,12 +1,53 @@
-// Declare app level module which depends on templates, and components
-angular.module('inspirr', [
-    'ui.router',
-    'angular-gestures'
-  /*,
-  'inspirr.main',
-  'inspirr.settings'*/
-])
-.config(function($stateProvider, $urlRouterProvider, hammerDefaultOptsProvider) {
+"use strict";
+var IR = {
+    // global scope
+};
+
+IR.MODULE_NAME = {
+    APP_NAME: "inspirr",
+    HEADER: IR.MODULE_NAME.APP_NAME + ".header",
+    CONTENT: IR.MODULE_NAME.APP_NAME + ".content",
+    LEFT_DRAWER: IR.MODULE_NAME.APP_NAME + ".leftDrawer",
+    RIGHT_DRAWER: IR.MODULE_NAME.APP_NAME + ".rightDrawer",
+    UTIL: IR.MODULE_NAME.APP_NAME + ".util",
+    // pages:
+    MAIN_PAGE: IR.MODULE_NAME.CONTENT + ".mainPage",
+    GALLERY_PAGE: IR.MODULE_NAME.CONTENT + ".galleryPage",
+    PROFILE_PAGE: IR.MODULE_NAME.CONTENT + ".profilePage",
+    SETTINGS_PAGE: IR.MODULE_NAME.CONTENT + ".settingsPage"
+};
+
+IR.MODULE = {
+    HEADER: angular.module(IR.MODULE_NAME.HEADER, []),
+    LEFT_DRAWER: angular.module(IR.MODULE_NAME.LEFT_DRAWER, []),
+    RIGHT_DRAWER: angular.module(IR.MODULE_NAME.RIGHT_DRAWER, []),
+    UTIL: angular.module(IR.MODULE_NAME.UTIL, []),
+    // pages:
+    MAIN_PAGE: angular.module(IR.MODULE_NAME.MAIN_PAGE, []),
+    GALLERY_PAGE: angular.module(IR.MODULE_NAME.GALLERY_PAGE, []),
+    PROFILE_PAGE: angular.module(IR.MODULE_NAME.PROFILE_PAGE, []),
+    SETTINGS_PAGE: angular.module(IR.MODULE_NAME.SETTINGS_PAGE, []),
+    // content module
+    CONTENT: angular.module(IR.MODULE_NAME.CONTENT, [
+        IR.MODULE_NAME.MAIN_PAGE,
+        IR.MODULE_NAME.GALLERY_PAGE,
+        IR.MODULE_NAME.PROFILE_PAGE,
+        IR.MODULE_NAME.SETTINGS_PAGE
+    ]),
+    // main module
+    INSPIRR: angular.module(IR.MODULE_NAME.APP_NAME, [
+        'ui.router',
+        'angular-gestures',
+        IR.MODULE_NAME.HEADER,
+        IR.MODULE_NAME.LEFT_DRAWER,
+        IR.MODULE_NAME.RIGHT_DRAWER,
+        IR.MODULE_NAME.CONTENT,
+        IR.MODULE_NAME.UTIL
+    ])
+};
+
+IR.MODULE.INSPIRR
+    .config(function($stateProvider, $urlRouterProvider, hammerDefaultOptsProvider) {
     // router config
     $urlRouterProvider.otherwise("/main");
     $stateProvider
