406d7dd7ef65226c46db2c253352aa840b864865
Demven <vinegar_dementor@mail.ru>
2015-12-23 21:22:25 +0300

    Section Partners: mobile;

--- assets/js/components/sections/partners.js
+++ assets/js/components/sections/partners.js
@@ -12,12 +12,13 @@ DAR.MODULE.SECTION_TEAM.directive('darSectionPartners', function($rootScope, $wi
                 SectionPartnersElementComponent.superclass.constructor.call(this);
 
                 this.NAME = "SectionPartners";
-                this.VERSION = "0.3";
+                this.VERSION = "0.5";
                 this.isDestroyOnPageChange = true;
                 this.isTriggerResize = true;
 
                 this.EVENT = {
-                    TAP: "touch"
+                    TAP: "touch",
+                    TOUCH_START: "touchstart"
                 };
 
                 this.VAL = {
@@ -29,7 +30,16 @@ DAR.MODULE.SECTION_TEAM.directive('darSectionPartners', function($rootScope, $wi
                 };
 
                 this.CLASS = {
-                    NORMAL: "normal"
+                    NORMAL: "normal",
+                    PARTNER_LOGO_HOVER: "hover"
+                };
+
+                this.SELECTOR = {
+                    PARTNER_LOGO: ".partner"
+                };
+
+                this.ELEMENT = {
+                    PARTNER_LOGOS: angular.element(wrapper[0].querySelectorAll(this.SELECTOR.PARTNER_LOGO))
                 };
 
                 this.CONFIG = {
@@ -41,7 +51,8 @@ DAR.MODULE.SECTION_TEAM.directive('darSectionPartners', function($rootScope, $wi
                     NORMAL: "normal"
                 };
 
-                var currentState;
+                var currentState,
+                    self = this;
 
                 var offScrollToSection = new Function();
 
@@ -61,6 +72,9 @@ DAR.MODULE.SECTION_TEAM.directive('darSectionPartners', function($rootScope, $wi
                 this._postCreate = function(){
                     // global listeners
                     offScrollToSection = $rootScope.$on(DAR.EVENT.WISH.SCROLL_TO_SECTION, angular.bind(this, this.onScrollToSectionEvent));
+
+                    // local listeners
+                    this.ELEMENT.PARTNER_LOGOS.on(this.EVENT.TOUCH_START, angular.bind(this, this.onTouchLogo));
                 };
 
                 this._resize = function(vw, vh) {
@@ -68,21 +82,32 @@ DAR.MODULE.SECTION_TEAM.directive('darSectionPartners', function($rootScope, $wi
                     if (vw > darDeviceInfo.MOBILE_WIDTH) {
                         // for desktop and tablet
                         fontSize = Math.min(parseFloat((vw / darDeviceInfo.DESKTOP_BASE_WIDTH).toFixed(2)), this.CONFIG.MAX_FONT_SIZE);
-                    }/* else {
+                    } else {
                         // mobile
-                        fontSize = Math.min(parseFloat((this.CONFIG.MAX_MOBILE_FONT_SIZE - (darDeviceInfo.MOBILE_WIDTH - vw) * 0.28 / 370).toFixed(2)), this.CONFIG.MAX_MOBILE_FONT_SIZE);
+                        fontSize = Math.min(parseFloat((this.CONFIG.MAX_MOBILE_FONT_SIZE - (darDeviceInfo.MOBILE_WIDTH - vw) * 0.35 / 370).toFixed(2)), this.CONFIG.MAX_MOBILE_FONT_SIZE);
                         // ^ here are some magic numbers: 370 - the difference between 690px and 320px (minimum mobile width)
-                        // 0.28 - difference in fonts values for these extreme width
-                    }*/
+                        // 0.35 - difference in fonts values for these extreme width
+                    }
                     wrapper.css(this.ATTR.FONT_SIZE, fontSize + this.VAL.REM);
                 };
 
                 this._destroy = function(){
                     // remove global listeners
                     offScrollToSection();
+
+                    // remove local listeners
+                    this.ELEMENT.PARTNER_LOGOS.off(this.EVENT.TOUCH_START);
                 };
 
                 /** *********************************************/
+
+                this.onTouchLogo = function(ev) {
+                    // remove hover-class from all logos
+                    self.ELEMENT.PARTNER_LOGOS.removeClass(self.CLASS.PARTNER_LOGO_HOVER);
+
+                    // add hover-class to touched logo
+                    angular.element(ev.target).addClass(self.CLASS.PARTNER_LOGO_HOVER);
+                };
 
                 this.onScrollToSectionEvent = function(ev, data) {
                     if (data.sectionName && data.sectionName === this.NAME) {
