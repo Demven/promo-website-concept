fe6e32efa5f9431e9c09376702b99325695be70d
Demven <vinegar_dementor@mail.ru>
2016-04-07 17:28:17 +0300

    Use https:443;

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require("express"),
     app = express(),
+    https = require('https'),
+    fs = require('fs'),
     path = require("path"),
     auth = require('./utils/auth'),
     configs = require(path.join(__dirname, "configs/configs.json"));
@@ -10,6 +12,10 @@ app.set("trust proxy", true);
 app.set("x-powered-by", false);
 app.set("port", process.env.PORT || 4000);
 
+var httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl/server.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/server.crt'))
+};
 
 app.use(express.static('public'));
 app.use(require("body-parser").json());
@@ -20,9 +26,10 @@ app.use(require("compression")());
 
 
 // Routes
-app.get("/", auth.basicAuth('dar', 'dar2016'), function(req, res) {
+app.get("/", /*auth.basicAuth('dar', 'dar2016'), */function(req, res) {
     res.sendFile(path.join(__dirname, '../public', 'dar.html'));
 });
 
-app.listen(app.get("port"));
-console.info("Server started on localhost:" + app.get("port"));
+//app.listen(app.get("port"));
+https.createServer(httpsOptions, app).listen(443);
+console.info("Server started on localhost: 443");
